4c11d30e457fc90d928736282417cc2bfbbe1192
Santiago Salinas <santisa147@gmail.com>
2022-08-29 21:33:46 -0300

    ValidacionIDstorage

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 class Producto{
     constructor(id,tipo,precio,cantidad){
-        this.Id = parseInt(id)
+        this.Id = id
         this.tipo = tipo
         this.precio = precio
         this.cantidad = cantidad
@@ -21,38 +21,61 @@ async function myPromise(){
 
 // Menu de opciones
 // let opc = 5
-
-let valorOpc
 let listaProductos = []
+let valorOpc
 let boton1 = document.getElementById('boton1')
 let opc = document.getElementById('opc')
 let tipoTxt = ''
+let encontrado = false
 
 boton1.onclick = () => {
-
+    listaProductos = []
+    listaProductos= listaProductos.concat(JSON.parse(localStorage.getItem('listaP')))
     console.log(opc.value)
     valorOpc= opc.value
     if (valorOpc == 1){
         let tituloTipo = document.getElementById('tituloTipo')
         let formProducto = document.getElementById('formProducto')
         tituloTipo.innerHTML = 'Agregar un Producto';
-        formProducto.innerHTML = '<h3>Id del producto:</h3><input type="text" id="idProducto"><h3>Precio:</h3><input type="text" id="precio"><h3>Cantidad:</h3><input type="text" id="cantidad"> \n<select name="tipo" id="tipo"><option>Panaderia</option><option>Almacen</option><option>Verduleria</option></select><input type="submit" id="boton2" value="submit">'
-        let idProducto = document.getElementById('idProducto')
+        formProducto.innerHTML = '<h3>Id del producto:</h3><input type="text" id="idProducto"><h3>Precio:</h3><input type="text" id="precio"><h3>Cantidad:</h3><input type="text" id="cantidad"> \n<select name="tipo" id="tipo"><option>Panaderia</option><option>Verduleria</option><option>Carniceria</option></select><input type="submit" id="boton2" value="submit">'
+        
         let boton2 = document.getElementById('boton2')
-        let tipo = document.getElementById('tipo');
-        let precioProducto = document.getElementById('precio')
-        let cantidadProducto = document.getElementById('cantidad')        
+               
         boton2.onclick = () =>{
+            let idProducto = document.getElementById('idProducto').value
+            idProducto = parseInt(idProducto)
+            let tipo = document.getElementById('tipo');
+            let precioProducto = document.getElementById('precio')
+            let cantidadProducto = document.getElementById('cantidad') 
             
-            Swal.fire({
-                icon: 'success',
-                text: 'Se agrego Correctamente su Producto'
-              })
+            localStorage.clear('listaP')
+            buscar(idProducto)
+            if (encontrado == true){
+                Swal.fire({
+                    icon:'error',
+                    text:'El id del producto ya existe'
+                })
+            }else{
+                if(tipo.selectedIndex == 0){tipoTxt = 'Panaderia'}
+                tipo.onchange = () =>{tipoTxt = tipo.options[tipo.selectedIndex].text }
+                if(listaProductos[0] == null){
+                    listaProductos[0]= new Producto(idProducto,tipoTxt, precioProducto.value, cantidadProducto.value)
+                }else{                                
+                listaProductos.push( new Producto(idProducto,tipoTxt, precioProducto.value, cantidadProducto.value))
+                }
+                console.log(idProducto +' ' + tipoTxt +' '+precioProducto.value+' '+cantidadProducto.value)
+                Swal.fire({
+                    icon: 'success',
+                    text: 'Se agrego Correctamente su Producto'
+                })
+            }
+            encontrado = false
+            localStorage.setItem('listaP',JSON.stringify(listaProductos))
+            
+
+            
               
-            tipo.onchange = () =>{tipoTxt = tipo.options[tipo.selectedIndex].text }
-            if(tipo.selectedIndex == 0){tipoTxt = 'Panaderia'}
-            listaProductos.push( new Producto(idProducto.value,tipoTxt, precioProducto.value, cantidadProducto.value))
-            console.log(idProducto.value +' ' + tipoTxt +' '+precioProducto.value+' '+cantidadProducto.value)
+            
             
         }
     }else if(valorOpc == 2){
@@ -100,11 +123,14 @@ function recorrer(array, funcion){
     }
 }
 function buscar(id){
-    for (i = 0; i <= listaProductos.length; i++){
-        if(listaProductos[i].Id == id){
-            return i
+    if(listaProductos[0] != null ){
+        for (i = 0; i < listaProductos.length; i++){
+            if(listaProductos[i].Id == id){
+                encontrado = true                
+            }
         }
     }
+   
 }
 
 function Comparacion(a, b){
@@ -117,4 +143,7 @@ function Comparacion(a, b){
     }
 
     
+}
+function LeerDatos(){
+    fetch('./datos.json')
 }
